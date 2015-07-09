0ed24defa236c7ff351eed4fea0b0eaf943f7d64
Hack Reactor Students <accounts@hackreactor.com>
2015-07-08 21:31:12 -0700

    Added two shia dancers with flipping capability for one when clicked.
    Added functionality to line up all dancers.

---
+++ src/shia2.js
@@ -0,0 +1,26 @@
+var makeMotivateShia = function(top, left, timeBetweenSteps){
+  //moved to top so oldStep is set before being overridden before we inherit from makeDancer
+  this.oldStep = makeDancer.prototype.step;
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+
+  this.$node = $('<span class="shia"></span>')
+  this.$node.html('<span class="front"><img src="images/shia3.gif"></span><span class="back"><img src="images/shia3.gif"></span>');
+
+  this.setPosition(top, left);
+  this.action();
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+makeMotivateShia.prototype = Object.create(makeDancer.prototype);
+
+makeMotivateShia.prototype.action = function(){
+  // / call the old version of step at the beginning of any call to this new version of step
+  this.$node.flip();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+};
+
+makeMotivateShia.prototype.constructor = makeMotivateShia;
